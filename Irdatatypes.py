5926c91fb4f980149268ea52bff6c74548d58109
EwaSzczerbaGitHub <ewa.szczerba@icloud.com>
2022-02-08 08:49:53 +0000

    Python commands

---
+++ Irdatatypes.py
@@ -0,0 +1,12 @@
+#var1 = 5
+#var2 = 7
+#
+#print(var1 + var2)
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
+
+gross_world_product = 84.84
