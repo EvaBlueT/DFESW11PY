c868dbb31f5200a4b322d2bfcfccfddb5a9b6639
EwaSzczerbaGitHub <ewa.szczerba@icloud.com>
2022-02-11 10:46:08 +0000

    more practise

--- files.py
+++ files.py
@@ -1,4 +1,31 @@
-filevar = open("files.txt")
-filecontents =filevar.read()
-filevar.close()
-print(filecontents)
+# filevar = open("filename.txt", "r")
+
+# outfile = ""
+
+# for line in range(1,10):
+#     if line % 2 == 0:
+#         outfile += filevar.readline()
+#     else:
+#         filevar.readline()
+
+
+# filevar = open("filename.txt", "w")
+# filevar.write(outfile)
+# filevar.close()
+        
+file = open("Teams.txt")
+filecontent = file.read()
+print(filecontent)
+
+# for line in range(1,6):
+#     if line == 1:
+#         print(filecontent)
+#     elif line == 4:
+#         print(filecontent)     
+
+
+
+
+# filecontents =filevar.read()
+# filevar.close()
+# print(filecontents)
